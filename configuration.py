b811741ac2b114d796b5f17d7e6aef3b32d29ce0
CARB85 <173071167+CARB85@users.noreply.github.com>
2024-09-07 17:16:00 -0400

    Actualización del archivo configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
-CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
-KITS_PATH = # Almacena la ruta para crear un kit en esta variable
+URL_SERVICE = "https://cnt-39ad4466-61a0-4648-b877-0aa345b84feb.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+CREATE_USER_PATH = "/users" # Almacena la ruta para crear un usuario o usuaria en esta variable
+KITS_PATH = "/kits" # Almacena la ruta para crear un kit en esta variable
